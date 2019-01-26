58b4cd32d165b7537f0827ae8045ccac7771f181
CybertekSchool <github@cybertekschool.com>
2019-01-26 16:51:35 -0500

    changed test to use pages utility and generate reports

--- src/test/java/com/weborders/tests/smoke_tests/PageHeadersTests.java
+++ src/test/java/com/weborders/tests/smoke_tests/PageHeadersTests.java
@@ -1,9 +1,5 @@
 package com.weborders.tests.smoke_tests;
 
-import com.weborders.pages.LoginPage;
-import com.weborders.pages.OrderPage;
-import com.weborders.pages.ViewAllOrdersPage;
-import com.weborders.pages.ViewAllProductsPage;
 import com.weborders.utilities.ApplicationConstants;
 import com.weborders.utilities.ConfigurationReader;
 import com.weborders.utilities.TestBase;
@@ -15,19 +11,30 @@ public class PageHeadersTests extends TestBase {
 
     @Test
     public void pageHeadersTest(){
-        LoginPage loginPage = new LoginPage();
-        ViewAllProductsPage viewAllProductsPage = new ViewAllProductsPage();
-        ViewAllOrdersPage viewAllOrdersPage = new ViewAllOrdersPage();
-        OrderPage orderPage = new OrderPage();
+        // create a new test / test case in the report and give name : Page headers test
+        extentLogger = report.createTest("Page headers test");
 
-        loginPage.login(ConfigurationReader.getProperty("username"),
+        // use logger to log the steps
+        extentLogger.info("Logging to the application");
+        pages.login().login(ConfigurationReader.getProperty("username"),
                 ConfigurationReader.getProperty("password"));
-        assertEquals(viewAllOrdersPage.header.getText(), ApplicationConstants.LIST_OF_ALL_ORDERS);
-        viewAllOrdersPage.viewAllProductsLink.click();
-        assertEquals(viewAllProductsPage.header.getText(), ApplicationConstants.LIST_OF_ALL_PRODUCTS);
-        viewAllProductsPage.orderLink.click();
-        assertEquals(orderPage.header.getText(), ApplicationConstants.ORDER);
 
+        extentLogger.info("Verifying header for all orders page");
+        assertEquals(pages.viewAllOrders().header.getText(), ApplicationConstants.LIST_OF_ALL_ORDERS);
+
+        extentLogger.info("Clicking on view all products link");
+        pages.viewAllOrders().viewAllProductsLink.click();
+
+        extentLogger.info("Verifying header for all products page");
+        assertEquals(pages.viewAllProducts().header.getText(), ApplicationConstants.LIST_OF_ALL_PRODUCTS);
+
+        extentLogger.info("Clicking on order link");
+        pages.viewAllProducts().orderLink.click();
+
+        extentLogger.info("Verifying header for order page");
+        assertEquals(pages.order().header.getText(), ApplicationConstants.ORDER);
+        // log the pass
+        extentLogger.pass("Page headers test");
 
     }
 
--- src/test/java/com/weborders/utilities/TestBase.java
+++ src/test/java/com/weborders/utilities/TestBase.java
@@ -17,8 +17,9 @@ public abstract class TestBase {
     protected Pages pages;
 
     protected static ExtentReports report;
-    protected static ExtentHtmlReporter htmlReporter;
+    private static ExtentHtmlReporter htmlReporter;
     protected static ExtentTest extentLogger;
+
 
     @BeforeMethod
     public void setupMethod() {
@@ -30,6 +31,8 @@ public abstract class TestBase {
 
     @AfterMethod
     public void tearDownMethod(ITestResult result) throws IOException {
+        // if any test fails, it can detect it,
+        // take a screen shot at the point and attach to report
         if (result.getStatus() == ITestResult.FAILURE) {
             String screenshotLocation = BrowserUtils.getScreenshot(result.getName());
             extentLogger.fail(result.getName());
@@ -45,19 +48,41 @@ public abstract class TestBase {
     @BeforeTest
     public void setUpTest() {
         report = new ExtentReports();
+        // this is our custom location of the report that will be generated
+        // report will be generated in the current project inside folder: test-output
+        // report file name: report.html
         String filePath = System.getProperty("user.dir") + "/test-output/report.html";
+
+//        windows users pls correct ur path:
+//        String filePath = System.getProperty("user.dir") + "\\test-output\\report.html";
+
+        // initialize the htmlReporter with the path to the report
         htmlReporter = new ExtentHtmlReporter(filePath);
 
+        // we attach the htmlreport to our report
         report.attachReporter(htmlReporter);
 
         report.setSystemInfo("Environment", "Staging");
         report.setSystemInfo("Browser", ConfigurationReader.getProperty("browser"));
         report.setSystemInfo("OS", System.getProperty("os.name"));
+
+        report.setSystemInfo("QA Engineer", "Admiral Kunkka");
+
         htmlReporter.config().setDocumentTitle("Prestashop Reports");
         htmlReporter.config().setReportName("Prestashop Automated Test Reports");
+
 //        htmlReporter.config().setTheme(Theme.DARK);
 
     }
+
+
+
+
+
+
+
+
+
 
     @AfterTest
     public void tearDownTest() {
